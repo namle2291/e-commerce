e9d8c66b4fc40979d15e2fd313d379b7497eadbb
lnam6507 <lnam6507@gmail.com>
2024-01-01 20:09:23 +0700

    changes

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGOO_URI);
-        console.log("Connect Success!!!");
-    } catch (error) {
-        console.log("Connect Failure!!!");
-    }
+  try {
+    await mongoose.connect(process.env.MONGOO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connect Success!!!");
+  } catch (error) {
+    console.log("Connect Failure!!!");
+  }
 }
 
 module.exports = connectDB;
